ebf17033102e4690019aa64647d60938661e598a
Mehmet Bozkurt <mehmet.64.b@gmail.com>
2025-05-17 16:15:17 +0300

    wordle eklendi

--- app/src/main/java/com/example/proje/WordDao.kt
+++ app/src/main/java/com/example/proje/WordDao.kt
@@ -10,16 +10,25 @@ import androidx.room.Query
 interface WordDao {
     @Insert
     suspend fun insert(Word:Word)
+
     @Query("DELETE FROM Word")
     suspend fun deleteAll()
+
     @Query("SELECT * FROM Word ORDER BY WordID ASC")
-   fun getAllWords(): LiveData<List<Word>>
+    fun getAllWords(): LiveData<List<Word>>
+
     @Query("SELECT * FROM Word ORDER BY WordID ASC")
     suspend fun getAllWordsNow(): List<Word>
+
     @Query("SELECT*FROM Word ORDER BY RANDOM() LIMIT :limit")
-   suspend fun getwordrand(limit:Int):List<Word>
-   @Delete
-   suspend fun delete(Word:Word)
-   @Query("SELECT *FROM Word WHERE EngWordName=:engwordname LIMIT 1")
-   suspend fun find(engwordname:String) :Word?
+    suspend fun getwordrand(limit:Int):List<Word>
+
+    @Delete
+    suspend fun delete(Word:Word)
+
+    @Query("SELECT *FROM Word WHERE EngWordName=:engwordname LIMIT 1")
+    suspend fun find(engwordname:String) :Word?
+
+    @Query("SELECT * FROM Word WHERE LENGTH(EngWordName) = 5")
+    suspend fun getWordsForWordle() : List<Word>
 }
